aea8d17bd4a16f629da45556e9e369337e182dee
amrish765-ux <amrish01011999@gmail.com>
2024-02-06 18:23:56 +0530

    kafka1

---
+++ springboot-kafka-tutorial/src/main/java/net/javaguides/springbootkafkatutorial/Controller/JsonMessageController.java
@@ -0,0 +1,25 @@
+package net.javaguides.springbootkafkatutorial.Controller;
+
+import net.javaguides.springbootkafkatutorial.kafka.JsonKafkaProducer;
+import net.javaguides.springbootkafkatutorial.payload.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/kafka")
+public class JsonMessageController {
+    private JsonKafkaProducer jsonKafkaProducer;
+
+    public JsonMessageController(JsonKafkaProducer jsonKafkaProducer) {
+        this.jsonKafkaProducer = jsonKafkaProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String>publish(@RequestBody User user){
+        jsonKafkaProducer.sendMessage(user);
+        return ResponseEntity.ok("Json Message sent to kafka topic");
+    }
+}
--- springboot-kafka-tutorial/src/main/java/net/javaguides/springbootkafkatutorial/kafka/JsonKafkaProducer.java
+++ springboot-kafka-tutorial/src/main/java/net/javaguides/springbootkafkatutorial/kafka/JsonKafkaProducer.java
@@ -27,4 +27,6 @@ public class JsonKafkaProducer {
                 .build();
         kafkaTemplate.send(message);
     }
+
+
 }
